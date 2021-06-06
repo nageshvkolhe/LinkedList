34f6731efd10081fe7409af61cd28e8cfd485b24
gitbash <kolhenagesh2017@gmail.com>
2021-06-06 19:15:12 +0530

    Added : Created Linked List by adding 30 and 56 to 70

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,9 @@
 package com.linkedlist;
 
 public class LinkedList {
-
 	public Node head = null;
 	public Node tail = null;
-	
+
 	class Node {
 		int data;
 		Node next;
@@ -13,22 +12,44 @@ public class LinkedList {
 			this.data = data;
 			this.next = null;
 		}
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
 	}
 
-	public void addNode(int data) {
-		Node newNode= new Node (data);
 
-		if(head == null) {
-			head = newNode;
-			tail = newNode;
+	public void addNode(int data) {
+		Node newNode= new Node (data);		
+
+		if(this.tail == null) {
+			this.tail = newNode;
 		}
-		else {
-			tail.next = newNode;
-			tail = newNode;
+		if(this.head == null) {
+			this.head = newNode;
+
+		} else {
+			Node tempNode = this.head;
+			this.head =newNode;
+			this.head.setNext(tempNode);
 		}
+
 	}
 
 	public void display() {
+		Node tempNode = head;
 		Node current = head;
 		if(head == null) {
 			System.out.println("List is Empty");
@@ -36,18 +57,19 @@ public class LinkedList {
 		}
 		System.out.println("Singly LinkedList of Node");
 		while(current != null) {
-			System.out.print(current.data + " " );
+			System.out.print(current.data + " ");
 			current = current.next;
 		}
+		System.out.println();
 	}
 
-	public static void main(String[] args){
-		LinkedListUC1 list = new LinkedListUC1();
+	public static void main(String[] args) {
+		LinkedListUC2 list = new LinkedListUC2();
 
-		list.addNode(56);
-		list.addNode(30);
 		list.addNode(70);
+		list.addNode(30);
+		list.addNode(56);
 
 		list.display();
-	} 
+	}
 }
